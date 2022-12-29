c03026c1f1fb870a4bbcdc505f83aea5e1f18a61
Anmol Chawla <anmol@dashtoon.com>
2022-12-29 19:09:34 +0530

    Add print for slack events request

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/events", app)
 @slack_events_adapter.on('message')
 def message(event, req):
     # ignore retries
+    print(event)
     if req.headers.get('X-Slack-Retry-Reason'):
         print("Ignoring Retry")
         return "Status: OK"
@@ -28,6 +29,7 @@ def message(event, req):
 @slack_events_adapter.on("reaction_added")
 def reaction_added(event, req):
     # ignore retries
+    print(event)
     if req.headers.get('X-Slack-Retry-Reason'):
         print("Ignoring Retry")
         return "Status: OK"
